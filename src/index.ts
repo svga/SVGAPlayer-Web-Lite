458ca06070ec9c50f2ed575570df26549c00f9f6
iulo <liulong0130@gmail.com>
2025-11-27 14:28:08 +0800

    fix: 导出常用枚举和类型

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ export { Player } from './player'
 
 // extension
 export { DB } from './db'
+
+// 常用枚举
+export { PLAYER_FILL_MODE, PLAYER_PLAY_MODE } from './types'
+
+// 常用类型
+export type { ParserConfigOptions, PlayerConfigOptions, PlayerConfig, DynamicElement, DynamicElements, ReplaceElement, ReplaceElements, Video, VideoSprite, VideoFrame, Rect, Transform } from './types'
